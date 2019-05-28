91a054258c2c2b572d6e161770a55a68aafa6397
Lisa Leslie <lisar@ibouge.com>
2019-05-27 19:22:47 -0700

    matches styling on profile info

--- src/Components/Matches.js
+++ src/Components/Matches.js
@@ -25,13 +25,13 @@ function Matches(props) {
         const matchesDivs = result.data.map((m) => 
         <table>
         <div key={m.id}>
-        <div> </div><div style={{display: "inline-block", float: "left"}}>
+        <div> </div><div style={{display: "inline-block", float: "left", paddingTop: "50px"}}>
                 <div className="profilePic"
                   style={{backgroundSize: "100%", width: "150px", height: "150px", backgroundRepeat: "no-repeat",  backgroundImage: "url('" + m.photo + "')"}}>
                   
                   
                   
-                </div> <div><button style={{backgroundColor: "Purple", color: "white", fontWeight: "bold", fontSize: "125%"}}onClick={()=> props.startChat(m)}>MESSAGE</button></div></div><tr><p><span style={{display: "inline-block"}}>{m.name} {m.age}{m.lang} {m.bio} {m.city} {m.country} {m.userName} {m.email}</span></p></tr>
+                </div> <div><button style={{backgroundColor: "Purple", color: "white", fontWeight: "bold", fontSize: "125%"}}onClick={()=> props.startChat(m)}>MESSAGE</button></div></div><tr><p><span style={{display: "inline-block", paddingTop: "30px", float: "left"}}><div style={{textAlign: "center", fontWeight: "bold", fontSize: "125%"}}>{m.name}</div><div><div style={{fontWeight: "bold"}}>{m.age}</div><div style={{textAlign: "center", marginLeft: "20px"}}>{m.country} from {m.city}  |  speaks {m.lang} </div> <p><div style={{display: "inline-block", float: "left", textAlign: "left", marginLeft: "25px"}}>{m.bio}</div></p> @{m.userName} </div></span></p></tr>
                 <div></div>
         </div>
         </table>
